56d2e99282d2855e6ed87932ce8a2baf24f22916
CluelessTimmy <timothyqhtran@gmail.com>
2017-10-10 18:33:18 +1100

    changed library controller to open catalogue menu with correct model

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -1,8 +1,13 @@
 package controller;
 
+
+
 import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import model.Library;
+import javafx.event.*;
 
 public class LibraryController extends Controller<Library> {
 
@@ -14,5 +19,11 @@ public class LibraryController extends Controller<Library> {
     public final Library getLibrary() {
         return model;
     }
-
+    
+    @FXML public void handleExit(ActionEvent event) throws Exception {
+    	stage.close();
+    	}
+    @FXML private void handleOpenCatalogue(ActionEvent event) throws Exception {
+        ViewLoader.showStage(getLibrary().getCatalogue(), "/view/catalogue.fxml", "Complete Catalogue", new Stage());
+    }
 }
