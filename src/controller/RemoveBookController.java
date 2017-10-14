5bd7c6a81fd04ee719ded329273c7d77c4ed1fa2
CluelessTimmy <timothyqhtran@gmail.com>
2017-10-15 00:21:22 +1100

    finish remove stages(book and patron)

--- src/controller/RemoveBookController.java
+++ src/controller/RemoveBookController.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class RemoveBookController {
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import au.edu.uts.ap.javafx.*;
+import model.*;
 
+public class RemoveBookController extends Controller<Catalogue>{
+	@FXML ListView<Book> booksLv;
+	
+	@FXML
+    public void initialize() {
+
+    }
+	private Book getBook() { return booksLv.getSelectionModel().getSelectedItem(); }
+	public final Catalogue getCatalogue() {
+		return model;
+	}
+	@FXML public void handleExit(ActionEvent event) throws Exception {
+    	stage.close();
+    	}
+	@FXML public void handleRemove(ActionEvent event) throws Exception {
+    	getCatalogue().removeBook(getBook());
+    	}
 }
--- src/controller/RemovePatronController.java
+++ src/controller/RemovePatronController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class RemovePatronController {
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import au.edu.uts.ap.javafx.*;
+import model.*;
 
+public class RemovePatronController extends Controller<Library>{
+	@FXML private ListView<Patron> patronsLv;
+	
+	@FXML
+    public void initialize() {
+
+    }
+	private Patron getPatron() { return patronsLv.getSelectionModel().getSelectedItem(); }
+	public final Library getLibrary() {
+        return model;
+    }
+	
+	@FXML public void handleExit(ActionEvent event) throws Exception {
+    	stage.close();
+    	}
+	@FXML public void handleRemove(ActionEvent event) throws Exception {
+		getLibrary().removePatron(getPatron());
+	}
 }
