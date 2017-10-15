a337f98f2870af78162857f81980684f565115eb
CluelessTimmy <timothyqhtran@gmail.com>
2017-10-15 16:37:12 +1100

    finished show all books stage

--- src/controller/CatalogueController.java
+++ src/controller/CatalogueController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
@@ -17,5 +18,8 @@ public class CatalogueController extends Controller<Catalogue> {
 	    }
 	 @FXML public void handleExit(ActionEvent event) throws Exception {
 	    	stage.close();
+	    	}
+	 @FXML public void handleShowAllBook(ActionEvent event) throws Exception {
+		 ViewLoader.showStage(getCatalogue(), "/view/showAllBooks.fxml", "Complete Catalogue", new Stage());
 	    	}
 }
--- src/controller/ShowAllBooksController.java
+++ src/controller/ShowAllBooksController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class ShowAllBooksController {
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import au.edu.uts.ap.javafx.*;
+import model.*;
 
+public class ShowAllBooksController extends Controller<Catalogue> {
+	
+	
+	@FXML
+	public void initialize() {
+		
+	}
+	public final Catalogue getCatalogue() {
+		return model;
+	}
+	@FXML public void handleExit(ActionEvent event) throws Exception {
+    	stage.close();
+    	}
 }
